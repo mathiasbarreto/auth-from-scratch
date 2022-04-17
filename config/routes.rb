c178152045575c8a8e33da478657cee36152fc66
mathiasbarreto <mathias.barreto@cashway.io>
2022-04-17 20:42:31 -0300

    adds confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+
   get 'static_pages/home'
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
