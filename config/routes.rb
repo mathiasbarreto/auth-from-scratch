fe50cd74054c3081dbfd8d7f54e1598bffb76d17
mathiasbarreto <mathias.barreto@cashway.io>
2022-04-17 20:04:29 -0300

    adds users controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
